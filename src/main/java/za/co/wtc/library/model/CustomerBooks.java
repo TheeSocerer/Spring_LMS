15ccf01daad515b1267f40aa2925700f7a321385
tshiburi123 <tshiburi123@student.wethinkcode.co.za>
2024-08-28 21:39:33 +0200

    added the code for the customerbooks table

--- src/main/java/za/co/wtc/library/model/CustomerBooks.java
+++ src/main/java/za/co/wtc/library/model/CustomerBooks.java
@@ -6,5 +6,51 @@ import javax.persistence.*;
 @Entity
 @Table(name = "customer_books")
 public class CustomerBooks {
+
+    @Column(name = "customer_id")
+    private long customerId;
+
+    @Column(name = "book_id")
+    private long bookId;
+
+    @Column(name = "date_issued")
+    private LocalDateTime dateIssued;
+
+    @Column(name = "due_date")
+    private LocalDateTime dueDate;
+
+    public CustomerBooks(){}
+
+    public long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(long customerId) {
+        this.customerId = customerId;
+    }
+
+    public long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(long bookId) {
+        this.bookId = bookId;
+    }
+
+    public LocalDateTime getDateIssued() {
+        return dateIssued;
+    }
+
+    public void setDateIssued(LocalDateTime dateIssued) {
+        this.dateIssued = dateIssued;
+    }
+
+    public LocalDateTime getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDateTime dueDate) {
+        this.dueDate = dueDate;
+    }
     
 }
